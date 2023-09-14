e40540a05c95dcf23349e9712ad91eb6887d2567
Alejandro Piñango <alejandropinango@gmail.com>
2023-09-13 22:22:49 -0300

    Se agregó el CDN de bootstrap y se creó el partial navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+ root "pages#index"
 end
