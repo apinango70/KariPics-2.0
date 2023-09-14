f5867fa85c20874d4f6884d642d0ca661af347c1
Alejandro Piñango <alejandropinango@gmail.com>
2023-09-13 22:19:51 -0300

    Se modificaron registration y application_controller y se agregó el enum de los roles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Enum de roles
+  enum role: { normal: 'normal',
+                admin: 'admin',
+  }, _default: 'normal'
+
 end
